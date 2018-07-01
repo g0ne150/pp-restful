671ab47e2e7790ff34913211265fb6651b1d1bae
高泽盼 <hzgaozepan@corp.netease.com>
2018-07-01 23:30:29 +0800

    FIX: header buffer

--- thrift/io/serialize.go
+++ thrift/io/serialize.go
@@ -3,7 +3,6 @@ package io
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
 
 	"git.apache.org/thrift.git/lib/go/thrift"
 )
@@ -33,21 +32,19 @@ func Serialize(tbase thrift.TStruct) ([]byte, error) {
 
 	headerbuff := HeaderSerialize(pri.header)
 
-	fmt.Println(headerbuff.Bytes())
-	fmt.Println(tTransport.Buffer.Bytes())
-
-	// FIXME 两个 buffer 拼接还有问题
+	// FIXME body buffer 的序列看起对的，比 nodejs 实现前面多了 0 0 0 39 猜测是 go 的零值导致的，感觉无碍，待查
+	// [0 0 0 39 24 8 97 103 101 110 116 32 105 100 24 16 97 112 112 108 105 99 97 116 105 111 110 32 110 97 109 101 22 0 24 0 54 0 38 0 52 0 0]
+	// nodejs: [ 24 8 97 103 101 110 116 32 105 100 24 16 97 112 112 108 105 99 97 116 105 111 110 32 110 97 109 101 102 1 37 0 100 0 0 ]
 	headerbuff.Write(tTransport.Buffer.Bytes())
 
 	return headerbuff.Bytes(), nil
 }
 
 func HeaderSerialize(header *Header) *bytes.Buffer {
-	buff := make([]byte, 4, 4)
+	buff := make([]byte, 4)
 
-	// FIXME 二进制拼接还是有问题
-	binary.PutVarint(buff[:1], int64(header.signature))
-	binary.PutVarint(buff[1:2], int64(header.version))
+	buff[0] = byte(header.signature)
+	buff[1] = byte(header.version)
 	binary.BigEndian.PutUint16(buff[2:], header.hType)
 
 	return bytes.NewBuffer(buff)
