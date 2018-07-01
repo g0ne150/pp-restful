fe18f040309c30923641afda13ca94f3dce6139e
高泽盼 <hzgaozepan@corp.netease.com>
2018-07-01 23:33:39 +0800

    delete: reference files; change: comments

--- thrift/io/serialize.go
+++ thrift/io/serialize.go
@@ -32,9 +32,6 @@ func Serialize(tbase thrift.TStruct) ([]byte, error) {
 
 	headerbuff := HeaderSerialize(pri.header)
 
-	// FIXME body buffer 的序列看起对的，比 nodejs 实现前面多了 0 0 0 39 猜测是 go 的零值导致的，感觉无碍，待查
-	// [0 0 0 39 24 8 97 103 101 110 116 32 105 100 24 16 97 112 112 108 105 99 97 116 105 111 110 32 110 97 109 101 22 0 24 0 54 0 38 0 52 0 0]
-	// nodejs: [ 24 8 97 103 101 110 116 32 105 100 24 16 97 112 112 108 105 99 97 116 105 111 110 32 110 97 109 101 102 1 37 0 100 0 0 ]
 	headerbuff.Write(tTransport.Buffer.Bytes())
 
 	return headerbuff.Bytes(), nil
--- thrift/io/serialize_test.go
+++ thrift/io/serialize_test.go
@@ -17,17 +17,9 @@ func TestSerialize(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// FIXME body buffer 的序列看起对的，与 nodejs 实现有差异，猜测是 go 的零值导致的，感觉无碍，待查
+	// [0 0 0 39 24 8 97 103 101 110 116 32 105 100 24 16 97 112 112 108 105 99 97 116 105 111 110 32 110 97 109 101 22 0 24 0 54 0 38 0 52 0 0]
+	// nodejs: [ 24 8 97 103 101 110 116 32 105 100 24 16 97 112 112 108 105 99 97 116 105 111 110 32 110 97 109 101 102 1 37 0 100 0 0 ]
 	fmt.Println("--------------->", buf)
-
-}
-
-func TestBinary(t *testing.T) {
-	var s int8 = io.SIGNATURE
-	var v int8 = 16
-	var hType uint16 = 40
-
-	fmt.Println(byte(s))
-	fmt.Println(byte(v))
-	fmt.Println(byte(hType))
 
 }
