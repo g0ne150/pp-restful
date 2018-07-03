78a534cb541b2d20160f1b9b21f4f4331ffc67e8
Zubin Gao <g0ne150@hotmail.com>
2018-07-03 19:10:20 +0800

    add: udp client

---
+++ socket/udp_client.go
@@ -0,0 +1,52 @@
+package socket
+
+import (
+	"errors"
+
+	"git.apache.org/thrift.git/lib/go/thrift"
+)
+
+type Serializer interface {
+	Serialize(tbase thrift.TStruct) ([]byte, error)
+}
+
+type UDPClient struct {
+	host       string
+	port       string
+	serializer Serializer
+}
+
+func NewUDPClient(host, port string) *UDPClient {
+	return &UDPClient{
+		host: host,
+		port: port,
+	}
+}
+
+func (c *UDPClient) SetHost(host string) {
+	c.host = host
+}
+
+func (c *UDPClient) SetPort(port string) {
+	c.port = port
+}
+
+func (c *UDPClient) SetSerializer(serializer Serializer) {
+	c.serializer = serializer
+}
+
+func (c *UDPClient) send(msg thrift.TStruct) (err error) {
+	if c.serializer == nil {
+		return errors.New("Set serializer first before call UDPClient.send() please")
+	}
+
+	var serializedMsg []byte
+	serializedMsg, err = c.serializer.Serialize(msg)
+	if err != nil {
+		return err
+	}
+
+	// TODO implement UDP socket send serialized data
+
+	return nil
+}
