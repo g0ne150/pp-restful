907ac069477a3f842b6db7ced59782819201e7d6
Zubin Gao <g0ne150@hotmail.com>
2018-07-07 23:04:33 +0800

    add: tests

--- thrift/io/serialize_test.go
+++ thrift/io/serialize_test.go
@@ -1,11 +1,14 @@
 package io
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"pp-restful/thrift/dto/trace"
 
 	"testing"
+
+	"git.apache.org/thrift.git/lib/go/thrift"
 )
 
 func TestSerialize(t *testing.T) {
@@ -25,28 +28,23 @@ func TestSerialize(t *testing.T) {
 		t.Errorf("buf length is zero")
 	}
 
-	// if buf[0] != 239 || buf[1] != 16 || buf[3] != 40 {
-	// 	t.Errorf("header buffer error")
-	// }
+	deTSpan := trace.NewTSpan()
+	deserializer := thrift.NewTDeserializer()
+	transport := thrift.NewTMemoryBuffer()
+	transport.Buffer = bytes.NewBuffer(buf[4:])
 
-	// deTSpan := trace.NewTSpan()
-	// deserializer := thrift.NewTDeserializer()
-	// tTransport := thrift.NewTMemoryBuffer()
-	// tTransport.Buffer = bytes.NewBuffer(buf[4:])
-	// transport := thrift.NewTFramedTransport(tTransport)
+	deserializer.Protocol = thrift.NewTCompactProtocol(transport)
 
-	// deserializer.Protocol = thrift.NewTCompactProtocol(transport)
+	buffer := []byte{}
+	err = deserializer.Read(deTSpan, buffer)
+	if err != nil {
+		t.Errorf("deserialize failed: %s", err.Error())
+	}
 
-	// buffer := []byte{}
-	// err = deserializer.Read(deTSpan, buffer)
-	// if err != nil {
-	// 	t.Errorf("deserialize failed: %s", err.Error())
-	// }
+	fmt.Println(deTSpan)
 
-	// fmt.Println(deTSpan)
-
-	// if deTSpan.ApplicationName != tSpan.ApplicationName || deTSpan.AgentId != tSpan.AgentId {
-	// 	t.Errorf("deserialize error")
-	// }
+	if deTSpan.ApplicationName != tSpan.ApplicationName || deTSpan.AgentId != tSpan.AgentId {
+		t.Errorf("deserialize error")
+	}
 
 }
